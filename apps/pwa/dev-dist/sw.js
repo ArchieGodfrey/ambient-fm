d7e21736fae165d0c7500da57f062109e2e5c925
ArchieGodfrey <archiegod@gmail.com>
2026-06-01 01:28:02 +0000

    Fix action

--- apps/pwa/dev-dist/sw.js
+++ apps/pwa/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5d155c7a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.bqeg8a0kb3g"
+    "revision": "0.9cs5qbbsg0s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
